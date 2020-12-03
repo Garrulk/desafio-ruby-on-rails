849bdbc91cdb8293c86f0d9344aa28c44e30224b
Garrulk <rodrigo.rsaal@gmail.com>
2020-12-02 21:24:26 -0300

    criando uma tela de login, adicionarndo o arquivo CNAB e escrevendo a documentação no arquivo Readme.md

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,4 @@
 class Transaction < ApplicationRecord
+  validates :kind, :transaction_date, :value, :cpf, :card_number,
+            :transaction_hour, :store_owner, :store_name, presence: true
 end
